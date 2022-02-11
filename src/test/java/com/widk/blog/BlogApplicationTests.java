e92beee53794957a460fad232ed87453465b6449
Widonggeon1 <widk0419@naver.com>
2022-02-11 16:06:03 +0900

    환경세팅완료

---
+++ src/test/java/com/widk/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widk.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
